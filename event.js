6d29e750c63e00fd2c269efa6eced1c76c940ffa
camilledtr <97107181+camilledtr@users.noreply.github.com>
2022-11-25 02:13:03 +0100

    design (TODO change rotation origin for .pendulum)
    change to: left center

--- event.js
+++ event.js
@@ -1,3 +1,20 @@
-var deg = document.getElementById("submit").value;
+const pendulum = document.querySelector('.pendulum');
+pendulum.addEventListener('mouseover', () => {
+    const element = document.querySelector('.pendulum');
+    element.style.animationPlayState = 'paused'
+});
 
-document.documentElement.style.setProperty('--deg-initialiser', deg);
+pendulum.addEventListener('mouseout', () => {
+    const element = document.querySelector('.pendulum');
+    element.style.animationPlayState = 'running'
+});
+
+
+
+
+
+
+
+// var deg = document.getElementById("deg").value;
+
+// document.documentElement.style.setProperty('--deg-initialiser', deg);
